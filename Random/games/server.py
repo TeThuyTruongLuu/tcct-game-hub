cd56739feb6041ad6de2274def61b2d0e777a9a9
TeThuyTruongLuu <tethuytruongluu1947@gmail.com>
2025-05-09 15:43:29 +0700

    Update server.py

--- Random/games/server.py
+++ Random/games/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, make_response, request, send_from_directory
 from flask_cors import CORS
 import os
-import epub
+from ebooklib import epub
 import logging
 
 # Cấu hình logging
