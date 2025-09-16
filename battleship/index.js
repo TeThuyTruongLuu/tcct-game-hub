620aeabd661491a389044d9a6e4c39c2a85295d2
TeThuyTruongLuu <tethuytruongluu1947@gmail.com>
2025-09-16 16:43:01 +0700

    Update index.js

--- battleship/index.js
+++ battleship/index.js
@@ -487,7 +487,7 @@ if (step === "battle") {
 		if (!roomData) return;
 		if (roomData.status === "ended" && !gameEnded) {
 			gameEnded = true;
-			await cleanupRoom();
+			cleanupRoom();
 		}
 
 		if (youEl.childElementCount === 0) buildBoard(youEl);
